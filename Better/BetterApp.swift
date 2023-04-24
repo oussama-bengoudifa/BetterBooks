2990b46f0a5c7a2592f83d11150289bdb6049652
oussama-bengoudifa <oussama.bengoudifa@smartinia.dz>
2023-04-24 20:57:06 +0100

    Initial Commit

---
+++ Better/BetterApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterApp.swift
+//  Better
+//
+//  Created by Bengoudifa Oussama on 24/4/2023.
+//
+
+import SwiftUI
+
+@main
+struct BetterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
